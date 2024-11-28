0ab599ef8aa925015e5e0975adc8dd4117dc4214
SabinaMB <sabina.marian.blaariu@gmail.com>
2024-11-28 16:14:34 +0100

    styling chatList

--- src/Components/ChatBotApp.jsx
+++ src/Components/ChatBotApp.jsx
@@ -1,24 +1,45 @@
 import React from 'react'
+import './ChatBotApp.css'
 
 const ChatBotApp = () => {
   return (
     <div className='chatApp'>
         <div className='chatList'>
             <div className='chatListHeader'>
-                <h2>Chat List</h2>
-                    <i className='bx bx-edit-alt new-chat'></i>
+                <h4>Chat List</h4>
+                    <i className='bx bx-edit-alt newChat'></i>
+            </div>
+            <div className='chatListItem active'>
+                <h5>Chat 20/11/24 1:54:23 pm</h5>
+                <i className='bx bx-x x'></i>
             </div>
             <div className='chatListItem'>
-                <h4>Chat 20/11/24 1:54:23 pm</h4>
-                <i className='bx bx-x circle'></i>
+                <h5>Chat 20/11/24 1:54:23 pm</h5>
+                <i className='bx bx-x x'></i>
             </div>
             <div className='chatListItem'>
-                <h4>Chat 20/11/24 1:54:23 pm</h4>
-                <i className='bx bx-x circle'></i>
+                <h5>Chat 20/11/24 1:54:23 pm</h5>
+                <i className='bx bx-x x'></i>
             </div>
-            <div className='chatListItem'>
-                <h4>Chat 20/11/24 1:54:23 pm</h4>
-                <i className='bx bx-x circle'></i>
+        </div>
+        <div className='chatWindow'>
+            <div className='chatTitle'>
+                <h3>Chat with Bot</h3>
+                <i className='bx-bx-arrow-back arrow'></i>
+            </div>
+            <div className='chat'>
+                <div className='prompt'>
+                    Hi, how are you? How can I help you today? <span>12:59:51 PM</span>
+                </div>
+                <div className='response'>
+                    I am fine, thank you. <span>12:59:51 PM</span>
+                </div>
+                <div className='typing'>Typing...</div>
+                <form className='msgForm'>
+                    <i className='fa-solid fa-face smile emoji'></i>
+                    <input type='text' className='msgInput' placeholder='Type a message' />
+                    <i className='fa-solid fa-paper-plane'></i>
+                </form>
             </div>
         </div>
     </div>
