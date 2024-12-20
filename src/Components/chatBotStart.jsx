f1eabc338d4f2de1c2383e47f64646ff08eb35dd
SabinaMB <sabina.marian.blaariu@gmail.com>
2024-12-20 16:03:55 +0100

    correcting the import of css

--- src/Components/chatBotStart.jsx
+++ src/Components/chatBotStart.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import './ChatBotStart.css'
-import botImage from '../assets/bot6.png'
+import React from "react";
+import "./chatBotStart.css";
+import botImage from "../assets/bot6.png";
 
-
-const chatBotStart = ({onStartChat}) => {
+const chatBotStart = ({ onStartChat }) => {
   return (
-    <div className='startPage'>
-        <img src={botImage} alt='bot' className='botImage'/>
-        <button className='startButton' onClick={onStartChat}>Let's chat</button>
+    <div className="startPage">
+      <img src={botImage} alt="bot" className="botImage" />
+      <button className="startButton" onClick={onStartChat}>
+        Let's chat
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default chatBotStart
+export default chatBotStart;
